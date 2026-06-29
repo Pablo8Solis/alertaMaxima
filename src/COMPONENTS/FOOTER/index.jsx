361a46b66129138ca257816bab182107462d864f
solis <pablosolishrt@gmail.com>
2026-06-29 16:06:42 -0600

    feat: clean unused imports, fix favicon and ignore vercel config

--- src/COMPONENTS/FOOTER/index.jsx
+++ src/COMPONENTS/FOOTER/index.jsx
@@ -1,10 +1,10 @@
-import { FooterStyles, ContactInfo, LegalLinks } from "./styles";
+import { FooterStyles } from "./styles";
 
 const Footer = () => {
 
     return (
-        <FooterStyles id="contacto">
-            <img src="/assets/img/log.svg" alt="" />
+        <FooterStyles id="contacto" role="contentinfo">
+            <img src="/assets/img/log.svg" alt="Logo de Alerta Máxima - Sistemas de seguridad y alarmas" />
         </FooterStyles>
     )
 }
--- src/COMPONENTS/Main/styles.js
+++ src/COMPONENTS/Main/styles.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import theme from "../../theme";
 
 const MainStyles = styled.main`
     width:100%;
